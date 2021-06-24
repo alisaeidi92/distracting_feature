938580d3c7ec6b2b373ee45a7c8a418920d5d446
Ali <59937735+alisaeidi92@users.noreply.github.com>
2021-06-24 10:49:29 -0700

    Update MADDPG.py

--- rl/MADDPG.py
+++ rl/MADDPG.py
@@ -43,33 +43,71 @@ def get_args():
 
     return args
 
+def fanin_init(size, fanin=None):
+	fanin = fanin or size[0]
+	v = 1. / np.sqrt(fanin)
+	return torch.Tensor(size).uniform_(-v, v)
+
 # define the actor network
 class Actor(nn.Module):
-    def __init__(self, args, agent_id):
+    def __init__(self, state_dim, action_dim, action_lim):
+        
+        #state_dim, action_dim and action_lim are dynamic inputs based on the agent ID iterator
+        
         super(Actor, self).__init__()
-        self.max_action = args.high_action
-        self.fc1 = nn.Linear(args.obs_shape[agent_id], 64)
-        self.fc2 = nn.Linear(64, 64)
-        self.fc3 = nn.Linear(64, 64)
-        self.action_out = nn.Linear(64, args.action_shape[agent_id])
+        
+        self.state_dim = state_dim
+		self.action_dim = action_dim
+		self.action_lim = action_lim
+
+		# network dim [state dim, 256, 128, 64, action dim]
+		
+		self.fc1 = nn.Linear(state_dim,256)
+		self.fc1.weight.data = fanin_init(self.fc1.weight.data.size()) #initializing random normalized weights for states network [layer1]
+
+		self.fc2 = nn.Linear(256,128)
+		self.fc2.weight.data = fanin_init(self.fc2.weight.data.size()) #initializing random normalized weights for actions network [layer1]
+
+		self.fc3 = nn.Linear(128,64)
+		self.fc3.weight.data = fanin_init(self.fc3.weight.data.size()) #initializing random normalized weights for actions network [layer1]
+
+		self.fc4 = nn.Linear(64,action_dim)
+		self.fc4.weight.data.uniform_(0,1)
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        actions = self.max_action * torch.tanh(self.action_out(x))
+        
+        x = F.relu(self.fc1(state))
+		x = F.relu(self.fc2(x))
+		x = F.relu(self.fc3(x))
+		action = F.sigmoid(self.fc4(x))
 
-        return actions
+        return action
 
 
 class Critic(nn.Module):
-    def __init__(self, args):
+    def __init__(self, state_dim, action_dim):
+        
         super(Critic, self).__init__()
-        self.max_action = args.high_action
-        self.fc1 = nn.Linear(sum(args.obs_shape) + sum(args.action_shape), 64)
-        self.fc2 = nn.Linear(64, 64)
-        self.fc3 = nn.Linear(64, 64)
-        self.q_out = nn.Linear(64, 1)
+        
+        self.state_dim = state_dim
+		self.action_dim = action_dim
+        
+        
+		self.fcs1 = nn.Linear(state_dim,256)
+		self.fcs1.weight.data = fanin_init(self.fcs1.weight.data.size()) 
+		self.fcs2 = nn.Linear(256,128)
+		self.fcs2.weight.data = fanin_init(self.fcs2.weight.data.size()) 
+
+		self.fca1 = nn.Linear(action_dim,128)
+		self.fca1.weight.data = fanin_init(self.fca1.weight.data.size()) 
+
+
+		self.fc2 = nn.Linear(256,128)
+		self.fc2.weight.data = fanin_init(self.fc2.weight.data.size()) 
+
+		self.fc3 = nn.Linear(128,1)
+		self.fc3.weight.data.uniform_(-EPS,EPS)
+        
 
     def forward(self, state, action):
         state = torch.cat(state, dim=1)
@@ -77,13 +115,18 @@ class Critic(nn.Module):
             action[i] /= self.max_action
         action = torch.cat(action, dim=1)
         x = torch.cat([state, action], dim=1)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        q_value = self.q_out(x)
-        return q_value
-   
+        
+        s1 = F.relu(self.fcs1(state))
+		s2 = F.relu(self.fcs2(s1))
+		a1 = F.relu(self.fca1(action))
+		x = torch.cat((s2,a1),dim=1)
 
+		x = F.relu(self.fc2(x))		#using relu activation for the critic (model for regression)
+		x = self.fc3(x)
+        
+        return x
+   
+############################################### ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 class Buffer:
     def __init__(self, args):
