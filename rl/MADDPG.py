27b25931fbbb5b1e1281d0e3f54c785bb8ea6e8f
Ali <59937735+alisaeidi92@users.noreply.github.com>
2021-06-27 15:55:23 -0700

    Update MADDPG.py

--- rl/MADDPG.py
+++ rl/MADDPG.py
@@ -57,29 +57,29 @@ class Actor(nn.Module):
         super(Actor, self).__init__()
         
         self.state_dim = state_dim
-		self.action_dim = action_dim
-		self.action_lim = action_lim
+	self.action_dim = action_dim
+	self.action_lim = action_lim
 
-		# network dim [state dim, 256, 128, 64, action dim]
+	# network dim [state dim, 256, 128, 64, action dim]
 		
-		self.fc1 = nn.Linear(state_dim,256)
-		self.fc1.weight.data = fanin_init(self.fc1.weight.data.size()) #initializing random normalized weights for states network [layer1]
+	self.fc1 = nn.Linear(state_dim,256)
+	self.fc1.weight.data = fanin_init(self.fc1.weight.data.size()) #initializing random normalized weights for states network [layer1]
 
-		self.fc2 = nn.Linear(256,128)
-		self.fc2.weight.data = fanin_init(self.fc2.weight.data.size()) #initializing random normalized weights for actions network [layer1]
+	self.fc2 = nn.Linear(256,128)
+	self.fc2.weight.data = fanin_init(self.fc2.weight.data.size()) #initializing random normalized weights for actions network [layer1]
 
-		self.fc3 = nn.Linear(128,64)
-		self.fc3.weight.data = fanin_init(self.fc3.weight.data.size()) #initializing random normalized weights for actions network [layer1]
+	self.fc3 = nn.Linear(128,64)
+	self.fc3.weight.data = fanin_init(self.fc3.weight.data.size()) #initializing random normalized weights for actions network [layer1]
 
-		self.fc4 = nn.Linear(64,action_dim)
-		self.fc4.weight.data.uniform_(0,1)
+	self.fc4 = nn.Linear(64,action_dim)
+	self.fc4.weight.data.uniform_(0,1)
 
     def forward(self, x):
         
         x = F.relu(self.fc1(state))
-		x = F.relu(self.fc2(x))
-		x = F.relu(self.fc3(x))
-		action = F.sigmoid(self.fc4(x))
+	x = F.relu(self.fc2(x))
+	x = F.relu(self.fc3(x))
+	action = F.sigmoid(self.fc4(x))
 
         return action
 
@@ -90,23 +90,23 @@ class Critic(nn.Module):
         super(Critic, self).__init__()
         
         self.state_dim = state_dim
-		self.action_dim = action_dim
+	self.action_dim = action_dim
         
         
-		self.fcs1 = nn.Linear(state_dim,256)
-		self.fcs1.weight.data = fanin_init(self.fcs1.weight.data.size()) 
-		self.fcs2 = nn.Linear(256,128)
-		self.fcs2.weight.data = fanin_init(self.fcs2.weight.data.size()) 
+	self.fcs1 = nn.Linear(state_dim,256)
+	self.fcs1.weight.data = fanin_init(self.fcs1.weight.data.size()) 
+	self.fcs2 = nn.Linear(256,128)
+	self.fcs2.weight.data = fanin_init(self.fcs2.weight.data.size()) 
 
-		self.fca1 = nn.Linear(action_dim,128)
-		self.fca1.weight.data = fanin_init(self.fca1.weight.data.size()) 
+	self.fca1 = nn.Linear(action_dim,128)
+	self.fca1.weight.data = fanin_init(self.fca1.weight.data.size()) 
 
 
-		self.fc2 = nn.Linear(256,128)
-		self.fc2.weight.data = fanin_init(self.fc2.weight.data.size()) 
+	self.fc2 = nn.Linear(256,128)
+	self.fc2.weight.data = fanin_init(self.fc2.weight.data.size()) 
 
-		self.fc3 = nn.Linear(128,1)
-		self.fc3.weight.data.uniform_(-EPS,EPS)
+	self.fc3 = nn.Linear(128,1)
+	self.fc3.weight.data.uniform_(-EPS,EPS)
         
 
     def forward(self, state, action):
