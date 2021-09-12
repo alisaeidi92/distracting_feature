b939e1454b5057d8a9a4296ee49e08433536c43f
Helen <helen.zheng@sjsu.edu>
2021-09-12 15:37:54 -0700

    new inference and GUI script

---
+++ inference_snippet_test.py
@@ -0,0 +1,252 @@
+
+import numpy as np
+
+import requests
+import os
+#import torchvision.models as models
+#from model.model_b3_p import Reab3p16
+import model_adjusted.model_b3_p as m
+#import model.model_b3_p as m
+import torch 
+import os
+import tensorflow as tf 
+import argparse
+import zipfile
+from data.load_data import load_data,Dataset
+from data import preprocess 
+import config
+from torch.autograd import Variable
+import torch.optim as optim
+from collections import OrderedDict
+
+import config
+
+# Adjusted from original load_data from github repository.
+def load_data_for_inference(args, data_dir, data_split, filename):
+    data_files = []
+    # data_dir = '../process_data/reason_data/reason_data/RAVEN-10000/'
+    # data_dir = "C:/Users/Hertz/Documents/SJSU Coursework/MS Project_big files/RAVEN 1000/center_single"
+    # filename = "RAVEN_0_train.npz"
+    # for subdir in os.listdir(data_dir):
+    #     for filename in os.listdir(data_dir + subdir):
+    #         if "npz" in filename:
+    #             data_files.append(data_dir + subdir + "/" + filename)
+    data_files.append(data_dir + "/" + filename)
+
+
+    df = [data_file for data_file in data_files if data_split in data_file and "npz" in data_file][:]
+
+
+    print("df", df)
+    #data_files = [data_file for data_file in data_files if data_split in data_file]
+    print("Nums of "+data_split+" : ", len(df))
+    # train_loader = torch.utils.data.DataLoader(Dataset(train_files), batch_size=args.batch_size, shuffle=True,num_workers=args.numwork)#
+    loader = torch.utils.data.DataLoader(Dataset(args,df), batch_size=args.batch_size, num_workers=args.numwork)
+    return loader
+
+# original load_data from original github repository. 
+def load_data(args, data_split, data_dir):
+    data_files = []
+    # data_dir = '../process_data/reason_data/reason_data/RAVEN-10000/'
+    data_dir = "C:/Users/Hertz/Documents/SJSU Coursework/MS Project_big files/RAVEN 1000/center_single"
+    # filename = "RAVEN_0_train.npz"
+    # for subdir in os.listdir(data_dir):
+    #     for filename in os.listdir(data_dir + subdir):
+    #         if "npz" in filename:
+    #             data_files.append(data_dir + subdir + "/" + filename)
+	
+    for filename in os.listdir(data_dir):
+        if "npz" in filename:
+            data_files.append(data_dir + "/" + filename)
+    data_files.append(data_dir + "/" + filename)
+
+
+    df = [data_file for data_file in data_files if data_split in data_file and "npz" in data_file][:]
+
+
+    print("df", df)
+    #data_files = [data_file for data_file in data_files if data_split in data_file]
+    print("Nums of "+data_split+" : ", len(df))
+    # train_loader = torch.utils.data.DataLoader(Dataset(train_files), batch_size=args.batch_size, shuffle=True,num_workers=args.numwork)#
+    loader = torch.utils.data.DataLoader(Dataset(args,df), batch_size=args.batch_size, num_workers=args.numwork)
+    return loader
+
+def run_inference(data_dir, data_split, filename):
+    torch.multiprocessing.freeze_support()
+    device = torch.device('cpu')
+    
+    weights_path = "C:\\Users\\Hertz\\Documents\\SJSU Coursework\\MS Project_big files\\git\\distracting_feature\\distracting_feature\\epochs\\epoch860"
+    
+    weights_path = "C:\\Users\\Hertz\\Documents\\SJSU Coursework\\MS Project_big files\\git\\distracting_feature\\distracting_feature\\epochs\\epoch860" # got ~20 percent here. 
+    
+    weights_path = "C:\\Users\\Hertz\\Documents\\SJSU Coursework\\MS Project_big files\\git\\distracting_feature\\distracting_feature\\epochs\\epochbest(73.6)"
+    
+    #model_path = "C:\\Users\\Hertz\\Documents\\SJSU Coursework\\MS Project_big files\\git\\distracting_feature\\distracting_feature\\epochs\\epoch860" # got ~70 percent here. 
+
+    #image_path = "C:\\Users\\sonam\\Desktop\\MS_Project\\test_model\\RAVEN_1368_test\\image.npy"
+    # image_path = os.path.join(RAVEN_folder, file_folder)
+    # image_path = str(RAVEN_folder)+"/"+ str(file)
+    # image_path = "C:/Users/Hertz/Documents/SJSU Coursework/MS Project_big files/RAVEN-10000-release/RAVEN-10000/center_single/RAVEN_10_train.npz"
+    image_path = "C:/Users/Hertz/Documents/SJSU Coursework/MS Project_big files/RAVEN-10000-release/RAVEN-10000/"
+    ap = argparse.ArgumentParser()
+    #ap.add_argument("type_loss", type=bool)
+    #ap.add_argument("image_path", type=str)
+    ap.add_argument('--type_loss', type=bool, default=True)
+    ap.add_argument('--image_path', type=str,default= image_path)
+    ap.add_argument('--regime', type=str, default='all')
+    ap.add_argument('--image_type', type=str, default='image')
+    ap.add_argument('--batch_size', type=int, default=1)
+    ap.add_argument('--numwork', dest='numwork', type=int, default=1)
+    args = ap.parse_args()
+
+    # args = config.get_args()
+    # args = args[0]
+    # weights_path = args.path_weight+"/"+args.load_weight
+
+    pretrained_dict = torch.load(weights_path, map_location = device) 
+    # print("pretrained_dict:", pretrained_dict)
+    r_model = m.Reab3p16(args)
+    model_dict = r_model.state_dict()                           ## https://pytorch.org/tutorials/recipes/recipes/what_is_state_dict.htmlA state_dict is an integral entity 
+    pretrained_dict1 = {}                                      ##..if you are interested in saving or loading models from PyTorch
+    for k, v in pretrained_dict.items():                      ##filter out unnecessary keys k
+        if k[:7]=="module.":
+            k = k[7:]
+        if k in model_dict:                                   ##only when keys match(like conv2D..and so forth)
+            pretrained_dict1[k] = v                   
+    model_dict.update(pretrained_dict1)                      ##overwrite entries in the existing state dict 
+    r_model.load_state_dict(model_dict) 
+
+    # print("pretrained_dict1:", pretrained_dict1)
+
+    with torch.no_grad():
+        r_model.eval()
+        accuracy_all = []
+        #data_dir = "C:/Users/Hertz/Documents/SJSU Coursework/MS Project_big files/RAVEN-10000-release/RAVEN-10000/"
+        #data_dir = "C:/Users/Hertz/Documents/SJSU Coursework/MS Project_big files/RAVEN 1000/"
+        loader_try = load_data_for_inference(args, data_dir, data_split, filename)
+
+        # print(loader_try)
+
+        
+
+        # data_split = "train"
+        # data_files = [image_path]
+        # print("datafiles: ", data_files)
+
+        # df = [data_file for data_file in data_files][:]
+        # print("df: ", df)
+        loader = torch.utils.data.DataLoader(Dataset(args,loader_try), batch_size=args.batch_size, num_workers=args.numwork)
+
+        # checkpoint = torch.load(model_path, map_location=device)
+        count = 0
+        for x, y, style,me in loader_try:
+            count = count +1
+            #print("count", count)
+            # print("style:", style)
+            x, y = Variable(x), Variable(y)
+            pred= r_model(x)
+            # print("pred:", pred)
+            pred = pred[0].data.max(1)[1]
+            
+            # print("pred:", pred)
+            # print("y",y)
+            correct = pred.eq(y.data).cpu().numpy()
+            accuracy = correct.sum() * 100.0 / len(y)
+            print("accuracy", accuracy)
+            accuracy_all.append(accuracy) 
+        accuracy_all = sum(accuracy_all) / len(accuracy_all)
+        print(accuracy_all)
+        print("pred:",pred.data)
+        print("y:",y.data)
+        print("count:", count)
+        return pred
+        
+
+def run():
+    torch.multiprocessing.freeze_support()
+    device = torch.device('cpu')
+    
+    weights_path = "C:\\Users\\Hertz\\Documents\\SJSU Coursework\\MS Project_big files\\git\\distracting_feature\\distracting_feature\\epochs\\epoch860"
+    
+    weights_path = "C:\\Users\\Hertz\\Documents\\SJSU Coursework\\MS Project_big files\\git\\distracting_feature\\distracting_feature\\epochs\\epoch860" # got ~20 percent here. 
+    
+    weights_path = "C:\\Users\\Hertz\\Documents\\SJSU Coursework\\MS Project_big files\\git\\distracting_feature\\distracting_feature\\epochs\\epochbest(73.6)"
+    
+    #model_path = "C:\\Users\\Hertz\\Documents\\SJSU Coursework\\MS Project_big files\\git\\distracting_feature\\distracting_feature\\epochs\\epoch860" # got ~70 percent here. 
+
+    #image_path = "C:\\Users\\sonam\\Desktop\\MS_Project\\test_model\\RAVEN_1368_test\\image.npy"
+    # image_path = os.path.join(RAVEN_folder, file_folder)
+    # image_path = str(RAVEN_folder)+"/"+ str(file)
+    # image_path = "C:/Users/Hertz/Documents/SJSU Coursework/MS Project_big files/RAVEN-10000-release/RAVEN-10000/center_single/RAVEN_10_train.npz"
+    image_path = "C:/Users/Hertz/Documents/SJSU Coursework/MS Project_big files/RAVEN-10000-release/RAVEN-10000/"
+    ap = argparse.ArgumentParser()
+    #ap.add_argument("type_loss", type=bool)
+    #ap.add_argument("image_path", type=str)
+    ap.add_argument('--type_loss', type=bool, default=True)
+    ap.add_argument('--image_path', type=str,default= image_path)
+    ap.add_argument('--regime', type=str, default='all')
+    ap.add_argument('--image_type', type=str, default='image')
+    ap.add_argument('--batch_size', type=int, default=1)
+    ap.add_argument('--numwork', dest='numwork', type=int, default=1)
+    args = ap.parse_args()
+
+    # args = config.get_args()
+    # args = args[0]
+    # weights_path = args.path_weight+"/"+args.load_weight
+
+    pretrained_dict = torch.load(weights_path, map_location = device) 
+    # print("pretrained_dict:", pretrained_dict)
+    r_model = m.Reab3p16(args)
+    model_dict = r_model.state_dict()                           ## https://pytorch.org/tutorials/recipes/recipes/what_is_state_dict.htmlA state_dict is an integral entity 
+    pretrained_dict1 = {}                                      ##..if you are interested in saving or loading models from PyTorch
+    for k, v in pretrained_dict.items():                      ##filter out unnecessary keys k
+        if k[:7]=="module.":
+            k = k[7:]
+        if k in model_dict:                                   ##only when keys match(like conv2D..and so forth)
+            pretrained_dict1[k] = v
+            print(k)                   
+    model_dict.update(pretrained_dict1)                      ##overwrite entries in the existing state dict 
+    r_model.load_state_dict(model_dict) 
+
+    # print("pretrained_dict1:", pretrained_dict1)
+
+    with torch.no_grad():
+        r_model.eval()
+        accuracy_all = []
+        #data_dir = "C:/Users/Hertz/Documents/SJSU Coursework/MS Project_big files/RAVEN-10000-release/RAVEN-10000/"
+        data_dir = "C:/Users/Hertz/Documents/SJSU Coursework/MS Project_big files/RAVEN 1000/"
+        loader_try = load_data(args,"train", data_dir)
+
+        # print(loader_try)
+
+        
+
+        # data_split = "train"
+        # data_files = [image_path]
+        # print("datafiles: ", data_files)
+
+        # df = [data_file for data_file in data_files][:]
+        # print("df: ", df)
+        loader = torch.utils.data.DataLoader(Dataset(args,loader_try), batch_size=args.batch_size, num_workers=args.numwork)
+
+        # checkpoint = torch.load(model_path, map_location=device)
+        count = 0
+        for x, y, style,me in loader_try:
+            count = count +1
+            #print("count", count)
+            # print("style:", style)
+            x, y = Variable(x), Variable(y)
+            pred= r_model(x)
+            # print("pred:", pred)
+            pred = pred[0].data.max(1)[1]
+            # print("pred:", pred)
+            # print("y",y)
+            correct = pred.eq(y.data).cpu().numpy()
+            accuracy = correct.sum() * 100.0 / len(y)
+            print("accuracy", accuracy)
+            accuracy_all.append(accuracy) 
+        accuracy_all = sum(accuracy_all) / len(accuracy_all)
+        print(accuracy_all)
+
+if __name__ == '__main__':
+    run()
---
+++ model_adjusted/model_b3_p.py
@@ -0,0 +1,307 @@
+import torch
+import torch.nn.functional as F
+
+from torch import nn
+
+# LEN model
+
+class Reab3p16(nn.Module):
+    def __init__(self,args):
+        super(Reab3p16, self).__init__()
+        self.NUM_PANELS = 16
+        self.type_loss=args.type_loss
+
+        # used to get embedding of individual panels
+        self.cnn = nn.Sequential(
+            nn.Conv2d(1, 32, 3, 2),     # (num_in_channels, num_out_channels, filter_size, stride_size)
+            nn.BatchNorm2d(32),         # (num_features)
+            nn.ReLU(),
+            nn.Conv2d(32, 32, 3, 2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, 3, 2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, 3, 2),
+            nn.BatchNorm2d(32),
+            nn.ReLU()
+        )
+
+        # used to get the embedding of all context panels combined
+        self.cnn_global = nn.Sequential(
+            nn.Conv2d(16, 32, 3, 2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, 3, 2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, 3, 2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, 3, 2),
+            nn.BatchNorm2d(32),
+            nn.ReLU()
+        )
+
+        # used to get embedding of individual panels
+        self.pre_g_fc = nn.Linear(32 * 9 ** 2, 256)         # (in_features, out_features)
+        self.pre_g_batch_norm = nn.BatchNorm1d(256)
+
+        # used to get the embedding of all context panels combined
+        self.pre_g_fc2 = nn.Linear(32 * 9 ** 2, 256)
+        self.pre_g_batch_norm2 = nn.BatchNorm1d(256)
+
+        # used in function g1 to get g-values of row and column ts
+        self.g = nn.Sequential(
+            nn.Linear(512+512, 1024),
+            nn.BatchNorm1d(1024),
+            nn.ReLU(),
+            nn.Linear(1024, 256*3),
+            nn.BatchNorm1d(256*3),
+            nn.ReLU(),
+            nn.Linear(256*3, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Dropout()
+        )
+
+        # used in function g2 to get g-values of non-row and non-column triplets
+        self.g2 = nn.Sequential(
+            nn.Linear(512 + 512, 1024),
+            nn.BatchNorm1d(1024),
+            nn.ReLU(),
+            nn.Linear(1024, 256 * 3),
+            nn.BatchNorm1d(256 * 3),
+            nn.ReLU(),
+            nn.Linear(256 * 3, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Dropout()
+        )
+
+        # function f
+        self.f = nn.Sequential(
+            nn.Linear(512, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.Linear(256, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Linear(256, 1)
+        )
+        
+        if self.type_loss:
+            # used to calculate type loss
+            self.meta_fc= nn.Sequential(
+                nn.Linear(512, 256),
+                nn.BatchNorm1d(256),
+                nn.ReLU(),
+                nn.Dropout(),
+                nn.Linear(256, 9)
+            )
+
+    # given a panel, returns a panel embedding
+    def comp_panel_embedding(self, panel):
+        batch_size = panel.shape[0]
+        panel = torch.unsqueeze(panel, 1)  # (batch_size, 160, 160) -> (batch_size, 1, 160, 160)
+        panel_embedding = self.cnn(panel)  # (batch_size, 1, 160, 160) -> (batch_size, 32, 9, 9)
+        panel_embedding = panel_embedding.view(batch_size, -1)
+        panel_embedding = self.pre_g_fc(panel_embedding)
+        panel_embedding = self.pre_g_batch_norm(panel_embedding)
+        panel_embedding = F.relu(panel_embedding)
+        return panel_embedding
+
+    # outputs row, col, and other triplets that don't include an answer panel
+    # objs: context panels
+    def panel_comp_obj_pairs(self, objs, batch_size):
+        
+        #device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") 
+        device = torch.device("cpu")
+        
+        obj_pairses_r = torch.zeros(batch_size, 2, 256 * 3, device = device)
+        obj_pairses_c = torch.zeros(batch_size, 2, 256 * 3, device = device)
+        obj_pairses = torch.zeros(batch_size, 54, 256 * 3, device = device)
+
+        count=0
+        index=0
+        for i in range(8):
+            for j in range(i):
+                for k in range(j):
+                    # if the panels are the first or second row, combine them into one triplet
+                    if ((7-i)==0 and ((7-j)==1) and ((7-k)==2)) or((7-i)==3 and ((7-j)==4) and ((7-k)==5)):
+                        obj_pairses_r[:, (7-i)//3, :] = torch.cat(
+                            [torch.cat([objs[:, 7 - i, :], objs[:, 7 - j, :]], 1), objs[:, 7 - k, :]], 1)
+                        count -= 1
+
+                    # if the panels are the first or second column, combine them into one triplet
+                    elif ((7-i)==0 and ((7-j)==3) and ((7-k)==6)) or((7-i)==1 and ((7-j)==4) and ((7-k)==7)):
+                        obj_pairses_c[:, 7-i, :] = torch.cat(
+                            [torch.cat([objs[:, 7 - i, :], objs[:, 7 - j, :]], 1), objs[:, 7 - k, :]], 1)
+                        count -= 1
+                    
+                    # else combine the panels into pairs that have need an answer panel (ex: row 3 and col 3)
+                    else:
+                        obj_pairses[:,count,:] = torch.cat([torch.cat([objs[:, 7 - i, :], objs[:, 7 - j, :]],1), objs[:, 7-k, :]], 1)
+                    #obj_pairs = torch.cat([torch.unsqueeze( objs[:,7-i,:],1),torch.unsqueeze( objs[:,7-j,:],1)],2)
+                    #obj_pairses[:,count,:] = torch.cat([obj_pairs, torch.unsqueeze(objs[:, 7-k, :], 1)], 2)
+                    count+=1
+        return obj_pairses, obj_pairses_c, obj_pairses_r
+
+
+    # outputs row, col, and other triplets that include an answer panel
+    # ans: answer panel embedding
+    # pan: embeddings of context panels
+    def ans_comp_obj_pairs(self, ans, pan, batch_size):
+        
+        #device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") 
+        device = torch.device("cpu")
+        
+        obj_pairses_r = torch.zeros(batch_size, 1, 256 * 3, device = device)
+        obj_pairses_c = torch.zeros(batch_size, 1, 256 * 3, device = device)
+        obj_pairses = torch.zeros(batch_size, 26, 256 * 3, device = device)
+
+        count=0
+        for i in range(8):
+            for j in range(i):
+                # if its the last column, combine the context panels with the ans panel to make a triplet
+                if (7-i)==2 and ((7-j)==5)  :
+                    obj_pairs = torch.cat([pan[:, 7 - i, :], pan[:, 7 - j, :]], 1)
+                    obj_pairses_c[:, 0, :] = torch.cat([obj_pairs, ans], 1)
+                    count -= 1
+
+                # if its the last row, combine the context panels with the ans panel to make a triplet
+                elif (7-i)==6 and ((7-j)==7) :
+                    obj_pairs = torch.cat([pan[:, 7 - i, :], pan[:, 7 - j, :]], 1)
+                    obj_pairses_r[:, 0, :] = torch.cat([obj_pairs, ans], 1)
+                    count -= 1
+
+                # else create triplets of other context panels and the ans panel
+                else:
+                    obj_pairs = torch.cat([pan[:, 7 - i, :], pan[:, 7 - j, :]], 1)
+                    obj_pairses[:,count,:] = torch.cat([obj_pairs, ans], 1)
+                count+=1
+        return obj_pairses, obj_pairses_c, obj_pairses_r
+
+    # function g1:
+    # used to get g-value of row and column triplets
+    def g_functin(self, context_pairs, panel_embedding_8, num_context_pairs, batch_size):
+        context_pairs = torch.cat([context_pairs, panel_embedding_8.repeat(1, num_context_pairs, 1)], 2)
+        context_pairs = context_pairs.view(batch_size * num_context_pairs, 1024)
+        context_g_out = self.g(context_pairs)
+        context_g_out = context_g_out.view(batch_size, num_context_pairs, 512)
+        context_g_out = context_g_out.sum(1)
+        return context_g_out
+
+    # function g2:
+    # used to get g-values of non-row and non-col triplets
+    def g_functin2(self, context_pairs, panel_embedding_8, num_context_pairs, batch_size):
+        context_pairs = torch.cat([context_pairs, panel_embedding_8.repeat(1, num_context_pairs, 1)], 2)
+        context_pairs = context_pairs.view(batch_size * num_context_pairs, 1024)
+        context_g_out = self.g2(context_pairs)
+        context_g_out = context_g_out.view(batch_size, num_context_pairs, 512)
+        context_g_out = context_g_out.sum(1)
+        return context_g_out
+
+    # main function
+    def forward(self, x):
+        """
+        general steps:
+        1. get embeddings of all context panels (8) and all answer panels(8)
+        2. get all combinations of triplets that don't require answer panels
+        3. get g-values of all triplets from step 2
+        4: combine all g-values from step 3 into one
+        5. get all combinations of triplets that require answer panels
+        6. get g-values of all triplets from step 5
+        7: combine all g-values from step 6 into one
+        8: combine both g-values from steps 4 and 7 into one
+        9: get f-score using g-value from step 8
+        10: output softmax of f-score, and type loss
+        """
+        
+        #device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") 
+        device = torch.device("cpu")
+        
+        batch_size = x.shape[0]
+        # placeholder for panel embeddings
+        panel_embeddings = torch.zeros(batch_size, self.NUM_PANELS, 256, device = device)
+
+        # an embedding of all panels (the yellow embedding in the diagram)
+        panel_embedding_8 = self.cnn_global(x[:, :, :, :])
+        panel_embedding_8 = self.pre_g_fc2(panel_embedding_8.view(batch_size, -1))
+        panel_embedding_8 = self.pre_g_batch_norm2(panel_embedding_8)
+        panel_embedding_8 = F.relu(panel_embedding_8)
+        panel_embedding_8 = torch.unsqueeze(panel_embedding_8, 1)
+
+        # get panel embeddings for all panels (8 context panels, 8 answer panels)
+        for panel_ind in range(self.NUM_PANELS):
+            panel = x[:, panel_ind, :, :]
+            panel_embedding = self.comp_panel_embedding(panel)
+            panel_embeddings[:, panel_ind, :] = panel_embedding
+        context_embeddings = panel_embeddings[:, :int(self.NUM_PANELS/2), :] # (batch_size, 8, 256)
+        answer_embeddings = panel_embeddings[:, int(self.NUM_PANELS/2):, :] # (batch_size, 8, 256)
+
+
+        num_context_pairs = 56
+        # Compute context pairs once to be used for each answer
+        obj_pairses, obj_pairses_c, obj_pairses_r = self.panel_comp_obj_pairs(context_embeddings, batch_size) # (batch_size, 56, 256*3)
+
+        # get g-values of all triplets that dont include answer panels
+        '''context_pairs = torch.cat([context_pairs, panel_embedding_8.repeat(1, num_context_pairs, 1)], 2)
+        context_pairs = context_pairs.view(batch_size * num_context_pairs, 1024)
+        context_g_out = self.g(context_pairs)
+        context_g_out = context_g_out.view(batch_size, num_context_pairs, 512)'''
+        context_g_out1 = self.g_functin2(obj_pairses, panel_embedding_8, 54, batch_size)
+        context_g_outr = self.g_functin(obj_pairses_r, panel_embedding_8, 2, batch_size)
+        context_g_outc = self.g_functin(obj_pairses_c, panel_embedding_8, 2, batch_size)
+        # combine g-values of row-triplets, col-triplets, and all other triplets into one
+        context_g_out = context_g_out1 + context_g_outc + context_g_outr
+
+        # placeholder for f-scores
+        f_out = torch.zeros(batch_size, int(self.NUM_PANELS/2), device = device)
+
+        # placeholder for type loss
+        if self.type_loss:
+            f_meta=torch.zeros(batch_size, 512, device = device)
+
+        for answer_ind in range(8):
+            # get individual answer panel embedding
+            answer_embedding = answer_embeddings[:, answer_ind, :] # (batch_size, 256)
+
+            # get row, column, and other triplets using current answer panel and all context panels
+            context_answer_pairs, context_answer_pairs_c, context_answer_pairs_r = self.ans_comp_obj_pairs(answer_embedding,context_embeddings, batch_size)# (batch_size, 28, 512)
+
+            # apply g-values for all answer panel triplets
+            '''context_answer_pairs = torch.cat([context_answer_pairs, panel_embedding_8.repeat(1, 28, 1)], 2)
+            context_answer_pairs = context_answer_pairs.view(batch_size * 28, 1024)
+            context_answer_g_out = self.g(context_answer_pairs) # (8, 512)
+            context_answer_g_out = context_answer_g_out.view(batch_size, 28, 512)
+            context_answer_g_out = context_answer_g_out.sum(1)'''
+            context_answer_g_out1 = self.g_functin2(context_answer_pairs, panel_embedding_8, 26, batch_size)
+            context_answer_g_outr = self.g_functin(context_answer_pairs_r, panel_embedding_8, 1, batch_size)
+            context_answer_g_outc = self.g_functin(context_answer_pairs_c, panel_embedding_8, 1, batch_size)
+
+            # combine g-values of row, column, and other triplets into one
+            context_answer_g_out = context_answer_g_out1 + context_answer_g_outc  + context_answer_g_outr
+
+            # combine context-panel-only and answer-panel g-values into one
+            g_out = context_g_out + context_answer_g_out
+
+            if self.type_loss:
+                f_meta+=g_out
+
+            # get f-score
+            f_out[:, answer_ind] = self.f(g_out).squeeze()
+
+        # output the softmax of the f-scores (and type-loss if asked)
+        if self.type_loss:
+            return F.log_softmax(f_out, dim=1),F.sigmoid(self.meta_fc(f_meta))
+        else:
+            #return F.log_softmax(f_out, dim=1)
+            return F.log_softmax(f_out, dim=1)
