2ce3d3575b69ad180a59a501b263cb4a6de6626f
Ali <59937735+alisaeidi92@users.noreply.github.com>
2021-10-05 09:59:51 -0700

    Update MADDPG.py

--- rl/MADDPG.py
+++ rl/MADDPG.py
@@ -9,6 +9,7 @@ import inspect
 import functools
 import threading
 import random
+import Agent
 
 
 
@@ -248,6 +249,13 @@ class MADDPG:
 
         for target_param, param in zip(self.critic_target_network.parameters(), self.critic_network.parameters()):
             target_param.data.copy_((1 - self.args.tau) * target_param.data + self.args.tau * param.data)
+	
+    def _init_agents(self):
+        agents = []
+        for i in range(self.args.n_agents):
+            agent = Agent(i, self.args)
+            agents.append(agent)
+        return agents
     """
     def get_exploitation_action(self, state,alpha_1):
 	
